d4eb3c79046ca228b96b7a9fc49e3e19f4f4b171
ecl5pse <fantamas20@gmail.com>
2019-11-25 15:05:05 +0200

    update file 	 modif function assembleAnArray

--- js/task3.js
+++ js/task3.js
@@ -5,8 +5,12 @@ function assembleAnArray(arr1 , arr2) {
 
     let arr3 = arr1.concat(arr2);
 
-    return console.log(arr3);
+    let arrUnique = arr3.filter(function (item , index) {
+        return arr3.indexOf(item )>= index;
+        
+    })
 
+    console.log(arrUnique);
 }
 
 
